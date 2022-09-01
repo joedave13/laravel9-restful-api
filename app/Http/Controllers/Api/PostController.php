da21c8595448fec5367734dc962544fe380f7b06
Joshua Davian <joshuadavian@gmail.com>
2022-09-01 12:55:48 +0700

    create post api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -14,5 +16,30 @@ class PostController extends Controller
         $posts = Post::query()->latest()->paginate(5);
 
         return new PostResource(true, 'Post list retrieved.', $posts);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required'],
+            'image' => ['required', 'image', 'mimes:jpeg,jpg,png,gif,svg', 'max:2048']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        $post = Post::query()->create([
+            'title' => $request->title,
+            'slug' => Str::slug($request->title),
+            'content' => $request->content,
+            'image' => $image->hashName()
+        ]);
+
+        return new PostResource(true, 'Post created successfully.', $post);
     }
 }
